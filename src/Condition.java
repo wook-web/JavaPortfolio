3ef251b7413a3bf2a2c96a5d297979ecba7924a9
JaeWook Lee <wooguui@gmail.com>
2026-01-16 08:23:22 +0900

    변수를 이용한 문장 출력 코드작성

--- src/Condition.java
+++ src/Condition.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Condition {
+    public static void main(String[] args) {
+
+        // 변수
+
+        int age1 = 20;
+        int age2 = 19;
+
+        //조건문
+        if (age1 >= 20) {
+                System.out.println("귀하의 나이는 " + age1 + "세 이며, " + "성인입니다.");
+        } else {
+            System.out.println("귀하의 나이는 " + age1 + "세 이며, " + "미성년자입니다.");
+        }
+
+        if (age2 >= 20) {
+            System.out.println("귀하의 나이는 " + age2 + "세 이며, " + "성인입니다.");
+        } else {
+            System.out.println("귀하의 나이는 " + age2 + "세 이며, " + "미성년자입니다.");
+        }
+    }
+
 }

50577aa966616f2d7ecb5757a984a2096f5cf3ba
JaeWook Lee <wooguui@gmail.com>
2026-01-16 07:54:24 +0900

    변수 학습 : int, double, String을 사용한 출력

---
+++ src/VariablePractice.java
@@ -0,0 +1,15 @@
+
+// 변수를 사용하여 출력하기
+
+public class VariablePractice {
+    public static void main(String[] args) {
+        int age = 20;            // int는 정수
+        double height = 175.5;   // double은 실수
+        String name = "홍길동";   // 문자열(글자)
+
+        System.out.println("이름 : " + name);
+        System.out.println("나이 : " + age + "세");
+        System.out.println("키 : " + height + "cm");
+
+    }
+}

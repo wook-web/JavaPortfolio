8fe754e5be0736450c07ef9654a52a64d1ae411c
JaeWook Lee <wooguui@gmail.com>
2026-01-17 02:24:17 +0900

    정수가 아닌 다른 수를 입력시 다른 문장을 출력

--- src/InputPractice.java
+++ src/InputPractice.java
@@ -9,9 +9,20 @@ public class InputPractice {
         Scanner sc = new Scanner(System.in); // Scanner 객체 생성 - 키보드(System.in)로 부터 입력을 받을 수 있는 도구 (sc)를 만듬.
 
         System.out.print("나이를 입력하세요. : ");
+
         int age = sc.nextInt(); // sc.nextInt() : 사용자 입력한 숫자
 
-        if (age <= 20) {
+        double age2 = sc.nextDouble(); // 궁금해서 해본 것
+
+        if (age2 < 0) {
+
+            System.out.println("정수를 입력해주세요.");
+
+        } else if (age2 > 0) {
+
+            System.out.println("정수를 입력해주세요.");
+
+        } else if (age <= 20) {
 
             System.out.println("성인입니다.");
 
@@ -24,3 +35,6 @@ public class InputPractice {
 }
 
 // 사용자가 입력한 숫자를 변수로 저장하고, if문으로 문장 출력
+
+// 정수가 아닌 실수를 입력 했을 시, if문으로 정수를 입력하라고 출력하게 코드 수정
+// * 코드에는 오류가 없으나, 입력을 2번 받아 의도와 맞지 않은 코드가 됨

b9595ef93498a8d0abaa3470a75c7faacb0fa124
JaeWook Lee <wooguui@gmail.com>
2026-01-17 05:35:18 +0900

    int과 nextInt를 String과 nextLine으로 코드 수정, matches와 Integer.parseInt(input)사용으로 숫자의 유무를 확인 할 수 있게 코드 수정

--- src/InputPractice.java
+++ src/InputPractice.java
@@ -6,29 +6,35 @@ import java.util.Scanner; // Scanner 사용시 필수
 public class InputPractice {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in); // Scanner 객체 생성 - 키보드(System.in)로 부터 입력을 받을 수 있는 도구 (sc)를 만듬.
+        Scanner sc = new Scanner(System.in); // sc = 변수, new Scanner = scanner를 새로 만듬, System.in = 키보드 입력
 
         System.out.print("나이를 입력하세요. : ");
 
-        int age = sc.nextInt(); // sc.nextInt() : 사용자 입력한 숫자
+        // int age = sc.nextInt(); : 사용자 입력한 숫자
 
-        double age2 = sc.nextDouble(); // 궁금해서 해본 것
+        String input =sc.nextLine(); // 사용자한테 문자열 받기 Ex) 1, 1.1, abc
 
-        if (age2 < 0) {
+        if (input.matches("\\d+")) {
 
-            System.out.println("정수를 입력해주세요.");
+            // input.matches("\\d+") - 숫자인지 확인
+            // "\d" - 숫자
+            // "+" - 하나 이상
 
-        } else if (age2 > 0) {
+            int age = Integer.parseInt(input); // 문자열 → 정수 / 변환
 
-            System.out.println("정수를 입력해주세요.");
+            if (age >= 20) {
 
-        } else if (age <= 20) {
+                System.out.println("성인입니다.");
 
-            System.out.println("성인입니다.");
+            } else {
+
+                System.out.println("미성년자입니다.");
+
+            }
 
         } else {
 
-            System.out.println("미성년자입니다.");
+            System.out.println("올바른 나이를 입력해주세요.");
 
         }
     }
@@ -37,4 +43,13 @@ public class InputPractice {
 // 사용자가 입력한 숫자를 변수로 저장하고, if문으로 문장 출력
 
 // 정수가 아닌 실수를 입력 했을 시, if문으로 정수를 입력하라고 출력하게 코드 수정
-// * 코드에는 오류가 없으나, 입력을 2번 받아 의도와 맞지 않은 코드가 됨
+// * 코드에는 오류가 없으나, 입력을 2번 받아 의도와 맞지 않은 코드가 됨 - 해결
+
+// 해결에 사용된 코드 :
+// String name =Sanner name.nextLine();
+// (input.matches("\\d+"))
+// int age = Integer.parseInt(input);
+
+// int 이름 = scanner 이름.nextInt(); - 정수 만 받기
+// double 이름 = scanner 이름.next.Double(); - 실수, 정수 받기
+// String 이름 = scanner 이름.nextLine(); - 한 줄 전체 받기

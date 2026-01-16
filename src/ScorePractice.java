ae0b77df9b3b045188eb6faa631f7097663547cf
JaeWook Lee <wooguui@gmail.com>
2026-01-16 22:14:39 +0900

    for문과 if문을 사용한 점수 등급 출력기 코드 완성

--- src/ScorePractice.java
+++ src/ScorePractice.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+
+// 점수 등급 출력기
 
 public class ScorePractice {
+
+    public static void main(String[] args) {
+
+        for (int score = 60; score <= 100; score += 10) {
+
+            if (score >= 100) {
+                System.out.println("점수 : " + score + " / 등급 : A+");
+            } else if (score >= 90) {
+                System.out.println("점수 : " + score + " / 등급 : A");
+            } else if (score >= 80) {
+                System.out.println("점수 : " + score + " / 등급 : B");
+            } else if (score >= 70) {
+                System.out.println("점수 : " + score + " / 등급 : C");
+            } else {
+                System.out.println("점수 : " + score + " / 등급 : F");
+            }
+        }
+
+        System.out.println();// 다른 방식의 코드
+
+        for (int score = 60; score <= 100; score += 10) {
+
+            String grade; // 변수
+
+            if (score >= 100) grade = "A+";
+            else if (score >= 90) grade = "A";
+            else if (score >= 80) grade = "B";
+            else if (score >= 70) grade = "C";
+            else grade = "F";
+
+            System.out.println("점수 : " + score + " / 등급 : " + grade);
+
+        }
+
+
+
+    }
 }

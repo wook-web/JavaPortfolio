9ba1d5a1b3e361412f6c538848964380495ae04c
JaeWook Lee <wooguui@gmail.com>
2026-01-17 08:46:23 +0900

    Random의 최대값과 최소값을 벗어 났을 때, 맞는 문장을 출력, Random의 최대값과 최소값을 벗어 났을 때, 맞는 문장을 출력

--- src/NumberGuessGame2.java
+++ src/NumberGuessGame2.java
@@ -14,13 +14,15 @@ public class NumberGuessGame2 {
 
         int answer = random.nextInt(10) + 1;    // int - answer은 (0 ~ 9) + 1중 무작위 값을 지님
 
+        int over = 10;
+
         boolean isCorrect = false;  // 예상 : 참과 거짓을 나타내는 변수
 
         System.out.println("숫자 맞추기 게임 시작!");
 
         while (!isCorrect) {    // while (조건)
 
-            System.out.print("1부터 10사이 숫자를 입력하세요.");
+            System.out.print("1부터 10사이 숫자를 입력하세요. : ");
 
             String input = scan.nextLine(); // 사용지의 입력값 받기 = input
 
@@ -28,18 +30,44 @@ public class NumberGuessGame2 {
 
                 int guess = Integer.parseInt(input);    // input는 int(정수)로 전환 = input
 
-                if (guess == answer) {
+                if (guess <= over) {
 
-                    System.out.println("정답입니다. 게임 종료");
+                    if (guess == answer) {  // 입력 값 = 랜덤 값
 
-                    isCorrect = true;    // 반복문 끝내기
+                        System.out.println("정답입니다. 게임 종료");
+
+                        isCorrect = true;    // 반복문 끝내기
+
+                    } else {
+
+                        System.out.println("틀렸습니다. 다시 시도하세요.");
+
+                        if (guess > answer) {   // 입력 값 > 랜덤 값
+
+                            System.out.println("너무 큽니다!");
+
+                            System.out.println("");
+
+                        } else if (guess < answer) {    // 입력 값 < 랜덤 값
+
+                            System.out.println("너무 적습니다!");
+
+                            System.out.println("");
+
+                        } else {
+
+                        }
+                    }
+
+                } else if (guess > over){
+
+                    System.out.println("10이하의 숫자를 입력하세요.");
+
+                    System.out.println("");
 
                 } else {
 
-                    System.out.println("틀렸습니다. 다시 시도하세요.");
-
                 }
-
 
             } else {
 
@@ -53,4 +81,7 @@ public class NumberGuessGame2 {
 
 }
 
-// 문재 - Random의 최대값과 최소값을 벗어 났을 때, 맞는 문장을 출력
+// 문재 : Random의 최대값과 최소값을 벗어 났을 때, 맞는 문장을 출력 - 해결
+// Random 값이 높은지 낮은지 알려줄 수 있도록 코드 수정
+
+// * 코드 개선이 필요해 보임

eb9752c06d779f7016dbd9464daec95bdb60528c
JaeWook Lee <wooguui@gmail.com>
2026-01-18 03:39:33 +0900

    코드 최하단부에 난이도 조정 코드 완성

--- src/NumberGuessGame2.java
+++ src/NumberGuessGame2.java
@@ -18,13 +18,13 @@ public class NumberGuessGame2 {
 
         int count = 0;  // 시도 횟수 카운트
 
-        int answer = random.nextInt(over) + 1;    // int - answer은 (0 ~ 9) + 1중 무작위 값을 지님
+        int answer = random.nextInt(over) + 1;  // int - answer은 (0 ~ 9) + 1중 무작위 값을 지님
 
         boolean isRunning = true;  // 참과 거짓을 나타내는 변수 - 제거
 
         System.out.println("숫자 맞추기 게임 시작!");
 
-        // 반복문
+        // 반복문 - 게임 플레이
 
         while (isRunning) {    // while (조건)
 
@@ -36,13 +36,13 @@ public class NumberGuessGame2 {
 
             // 1. 숫자인지 검사
 
-            if (!input.matches("\\d+")) {   // 만약 input이 정수와 매치된다면
+            if (!input.matches("\\d+")) {   // 만약 input이 정수와 매치가 안 된다면
 
                 System.out.println("정수를 입력해주세요.");
 
                 System.out.println();
 
-                continue; // wile 처음으로 돌아감
+                continue; // while 처음으로 돌아감
             }
 
             int guess = Integer.parseInt(input);    // input는 int(정수)로 전환 = input
@@ -132,3 +132,74 @@ public class NumberGuessGame2 {
 // 다시하기 기능 코드 관성
 // n을 입력하여도 종료가 안됨, 같은 숫자로 다시 시작 - 해결
 // boolean 변수를 추가하여 해결 + random.nextInt 추가로 재시작 시, 정답이 다시 랜덤한 값
+
+    while(true) {
+
+        System.out.print("난이도를 선택해주세요. : ");
+
+        String lv = scan.next.String
+
+        if (!lv.metches("\\d+")) {
+
+            int love = Integer.parseInt(lv);
+
+        } else {
+
+            System.out.print("1부터 3사이의 숫자를 입력하세요.");
+
+            System.out.print("");
+
+            continue;
+
+        }
+
+        if (lv.metches("\\d+")){
+
+            int love = Integer.parseInt(lv);
+
+            if(love =  1){
+
+                over =10;
+
+                break;
+
+            }else if(love = 2){
+
+                over =20;
+
+                break;
+
+            }else if(love = 3){
+
+                over =30;
+
+                break;
+
+            }else{
+
+                System.out.print("1부터 3사이의 숫자를 입력하세요.");
+
+                System.out.print("");
+
+            }
+
+        }
+
+
+
+
+
+        if (!lv.matches("\\d+")) {   // 만약 input이 정수와 매치가 안 된다면
+
+            System.out.println("정수를 입력해주세요.");
+
+            System.out.println();
+
+            continue; // while 처음으로 돌아감
+
+        }
+
+
+
+    }
+

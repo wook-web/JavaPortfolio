21db5e75264b22802969c7a729d257084857894d
JaeWook Lee <wooguui@gmail.com>
2026-01-26 19:32:36 +0900

    if문 위치를 상단의 for문으로 이동 - 가독성 향상

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -17,21 +17,17 @@ public class ArrayPractice {
 
             all += A[X];
 
+            if (max < A[X]) {
+
+                max = A[X];
+
+            }
+
         }
 
         System.out.println("합계 : " + all);
 
         System.out.println("평균 : " + all / A.length);
-
-        for (int Y = 0; Y < A.length ; Y++) {
-
-            if (max < A[Y]) {
-
-                max = A[Y];
-
-            }
-
-        }
 
         System.out.println("최댓값 : " + max);
 
@@ -40,7 +36,9 @@ public class ArrayPractice {
 }
 
 // for문을 활용하여, 배열 안의 값을 모두 출력
+
 // 배열의 평균값 출력 - 코드 수정 팔요 | *완료
+
 // for을 활용하여 합계값을 변수에 저장
 
 /* 구상
@@ -62,3 +60,5 @@ public class ArrayPractice {
    */
 
 // for문을 활용하여 배열의 최댓값 출력 코드 완성
+
+// if문 위치를 상단의 for문으로 이동 - 가독성 향상

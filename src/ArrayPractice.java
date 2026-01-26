231d443c36106d799af5e2423d18c93c7a3b015e
JaeWook Lee <wooguui@gmail.com>
2026-01-26 18:36:32 +0900

    for문을 활용하여 배열에 안의 값을 출력하는 코드 작성

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,2 +1,20 @@
+
+// 배열 학습
+
 public class ArrayPractice {
+
+    public static void main (String[] args){
+
+        int[] A = {70, 85, 90, 60, 100}; // 배열 생성
+
+        for (int X = 0; X < A.length; X++) {
+
+            System.out.println(A[X]);
+
+        }
+
+    }
+
 }
+
+// for문을 활용하여, 배열 안의 값을 모두 출력

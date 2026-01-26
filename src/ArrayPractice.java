36693f711a707695a4d491bc5f293f2cc3a0db1b
JaeWook Lee <wooguui@gmail.com>
2026-01-26 19:22:05 +0900

    배열값 중 최댓값 출력 코드 구상

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -17,7 +17,19 @@ public class ArrayPractice {
 
         }
 
-        System.out.println(all / A.length);
+        System.out.println("합계 : " + all);
+
+        System.out.println("평균 : " + all / A.length);
+
+        for (int Y = 0; Y < A.length ; Y++) {
+
+            if (>) {
+
+                
+
+            }
+
+        }
 
     }
 
@@ -26,3 +38,21 @@ public class ArrayPractice {
 // for문을 활용하여, 배열 안의 값을 모두 출력
 // 배열의 평균값 출력 - 코드 수정 팔요 | *완료
 // for을 활용하여 합계값을 변수에 저장
+
+/* 구상
+
+   배열의 갯수는 5개 비교해야되니까
+
+   5번 반복 for문 사용
+
+   A[0]이 기존 최댓값 변수보다 높으면 추가
+
+   배열값이 바뀌니까 for문의 인덱스 값 사용
+
+   0은 Y로
+
+   if(A[Y]>최대)
+
+   최대 = A[Y]
+
+   */

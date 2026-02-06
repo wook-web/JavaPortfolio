26d7689c873d86c2ced270af5a9dc886faad9fa6
JaeWook Lee <wooguui@gmail.com>
2026-02-06 20:45:43 +0900

    예제 코드 타이핑

---
+++ src/Method.java
@@ -0,0 +1,22 @@
+
+// 메서드 학습
+
+public class Method {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 7, 2, 9, 5};
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            sum += arr[i];
+
+        }
+
+        System.out.println("합계 : " + sum);
+
+    }
+
+}
